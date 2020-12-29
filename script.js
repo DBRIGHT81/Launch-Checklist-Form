02418e087ee09784ace2f8231f3092cb92859c7d
Demetrius Bright <demetriusmbright@gmail.com>
2020-12-29 00:33:24 -0600

    finished assignment

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// Write your JavaScript code here!
-window.addEventListener("load", function(){
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-               response.json().then( function(json) {
-                  const div = document.getElementById("missionTarget");
-                  // Add HTML that includes the JSON data
-                  div.innerHTML = `
-                  <h2>Mission Destination</h2>
-                  <ol>
-                     <li>Name: ${json[0].name}</li>
-                     <li>Diameter: ${json[0].diameter}</li>
-                     <li>Star: ${json[0].star}</li>
-                     <li>Distance from Earth: ${json[0].distance}</li>
-                     <li>Number of Moons: ${json[0].moons}</li>
-                  </ol>
-                  <img src="${json[0].image}">
-                  `;
+ // Write your JavaScript code here!
+ window.addEventListener("load", function() {  
+     fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+   response.json().then(function(json){         
+      let missionTarget = document.getElementById("missionTarget");
+      missionTarget.innerHTML= "<h2>Mission Destination</h2>";   
+
+      let i = Math.floor(Math.random() * Math.floor(5));         
+      let ol  = document.createElement('ol');         
+      
+      let li = document.createElement('li');
+      li.textContent= `Name: ${json[i].name}`;
+      ol.appendChild(li);  
+
+      li = document.createElement('li');
+      li.textContent= `Diameter: ${json[i].diameter}`;
+      ol.appendChild(li);    
+
+      li = document.createElement('li');
+      li.textContent= `Star: ${json[i].star}`;
+      ol.appendChild(li);
+
+      missionTarget.appendChild(ol);         
+      li = document.createElement('li');
+      li.textContent= `Distance from Earth: ${json[i].distance}`;
+      ol.appendChild(li);        
+
+      li = document.createElement('li');
+      li.textContent= `Number of Moons: ${json[i].moons}`;
+      ol.appendChild(li); 
+
+       let img = document.createElement('img');
+      img.src=`${json[i].image}`;
+      ol.appendChild(img);  
+          });
+   });
+               
    
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event){
